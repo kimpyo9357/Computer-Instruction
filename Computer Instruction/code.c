188462bd04d3cef21e1c4167572835b9052d3e09
kimpyo9357 <kimpyo9357@naver.com>
2022-05-05 13:38:19 +0900

    make print

--- Computer Instruction/code.c
+++ Computer Instruction/code.c
@@ -8,7 +8,7 @@ int R[32];
 int Memory[0x4000000];
 int instruction;
 int cycle;
-int format_type[3] = { 0 };
+int count[5] = { 0 }; // R type / I type / J type / Memory Access / branche
 
 struct instruction_ {
 	int instruction;
@@ -41,7 +41,7 @@ int main() {
 	int i = 0;
 
 
-	fp = fopen("input4.bin", "rb");
+	fp = fopen("gcd.bin", "rb");
 	while (1) {
 		ret = fread(&value, sizeof(value), 1, fp);
 		if (ret != 1) { break; }
@@ -53,26 +53,27 @@ int main() {
 	pc = 0;
 	R[31] = -1;
 	R[29] = 0x1000000;
-	for (i = 0; i < 32; i++) { printf("%x ", R[i]); }
 	printf("\n\n");
 	while (1) {
+		if (pc == -1) { break; }
 		fetch();
+		printf("Mem[%d] 0x%08X\n", pc, Memory[pc/4]);
 		update_pc();
 		in = decode(instruction);
 		value = execute(in);
 		value = memory(in, value);
 		writeback(in, value);
-		//Sleep(1);
+		printf("Register state : \n");
 		for (i = 0; i < 32; i++) { printf("%x ", R[i]); }
-		printf("\npc : %x\n\n",pc);
-		if (pc == -1) { break; }
-
-
-		//print();
+		printf("\n");
+		printf("pc : %d", pc);
+		if (in.opcode == 0x23) printf(", Memory address : %x, value :%d", R[in.rs] + in.simm, R[in.rt]);
+		printf("\n\n");
 		cycle++;
 	}
-	printf("cycle : %d\n", cycle);
-	printf("definitely quit");
+
+	printf("result : %d\n", R[2]);
+	print();
 	fclose(fp);
 	return 0;
 }
@@ -138,9 +139,9 @@ struct instruction_ decode(int inst_binary) {
 	strcat(target[3], "00");
 	ret.jtarget = strtoul(target[3], NULL, 2);
 	
-	printf("dec: opcode: %x shamt : %d funct: %x \n", ret.opcode, ret.shamt, ret.funct);
-	printf("dec: rs: %d rt %d rd : %d \n", ret.rs, ret.rt, ret.rd);
-	printf("dec: simm: %x btarget : %x jtarget: %x\n", ret.simm, ret.btarget, ret.jtarget);
+	//printf("dec: opcode: %x shamt : %d funct: %x \n", ret.opcode, ret.shamt, ret.funct);
+	//printf("dec: rs: %d rt %d rd : %d \n", ret.rs, ret.rt, ret.rd);
+	//printf("dec: simm: %x btarget : %x jtarget: %x\n", ret.simm, ret.btarget, ret.jtarget);
 
 	return ret; 
 };
@@ -161,6 +162,7 @@ int execute(struct instruction_ in) {
 		}
 		case 0x8: {
 			pc = R[in.rs];
+			count[3]++;
 			break;
 		}
 		case 0x27: {
@@ -191,75 +193,78 @@ int execute(struct instruction_ in) {
 		}
 		default: printf("Not In Instance");
 		}
-		format_type[0]++;
+		count[0]++;
 		//printf("R type\n");
 		break;
 	}
 	case 0x2:
-	case 0x3: {
+	case 0x3: {		
 		switch (in.opcode) {
-		case 0x2: {
-			pc = in.jtarget;
-			break;
-		}
-		case 0x3: {
-			value = pc + 4;
-			pc = in.jtarget;
-			break;
-		}
-		default: printf("Not In Instance");
-		}
+			case 0x2: {
+				pc = in.jtarget;
+				break;
+			}
+			case 0x3: {
+				value = pc + 4;
+				pc = in.jtarget;
+				break;
+			}
+			default: printf("Not In Instance");
+			}
 		//printf("J type\n");
-		format_type[2]++;
+		count[2]++;
+		count[3]++;
 		break;
 	}
 	default: {
 		switch (in.opcode) {
-		case 0x8:
-		case 0x9: {
-			value = R[in.rs] + in.simm;
+			case 0x8:
+			case 0x9: {
+				value = R[in.rs] + in.simm;
+				break;
+			}
+			case 0xc: {
+				value = R[in.rs] & in.zimm;
+				break;
+			}
+			case 0x4: {
+				if (R[in.rs] == R[in.rt]) { pc = pc + in.btarget; }
+				count[4]++;
+				break;
+			}
+			case 0x5: {
+				if (R[in.rs] != R[in.rt]) { pc = pc + in.btarget; }
+				count[4]++;
+				break;
+			}
+			case 0xf: {
+				value = in.zimm << 16;
+				break;
+			}
+			case 0x23: {
+				value = R[in.rs] + in.simm;
+				break;
+			}
+			case 0xd: {
+				value = R[in.rs] | in.zimm;
+				break;
+			}
+			case 0xa:
+			case 0xb: {
+				value = (R[in.rs] < in.simm) ? 1 : 0;
+				break;
+			}
+			case 0x2b: {
+				value = R[in.rt];
+				break;
+			}
+			default:
+				printf("Not In Instance");
+			}
+			count[1]++;
+			//printf("I type\n");
 			break;
 		}
-		case 0xc: {
-			value = R[in.rs] & in.zimm;
-			break;
-		}
-		case 0x4: {
-			if (R[in.rs] == R[in.rt]) { pc = pc + in.btarget; }
-			break;
-		}
-		case 0x5: {
-			if (R[in.rs] != R[in.rt]) { pc = pc + in.btarget; }
-			break;
-		}
-		case 0xf: {
-			value = in.zimm << 16;
-			break;
-		}
-		case 0x23: {
-			value = R[in.rs] + in.simm;
-			break;
-		}
-		case 0xd: {
-			value = R[in.rs] | in.zimm;
-			break;
-		}
-		case 0xa:
-		case 0xb: {
-			value = (R[in.rs] < in.simm) ? 1 : 0;
-			break;
-		}
-		case 0x2b: {
-			value = R[in.rt];
-			break;
-		}
-		default:
-			printf("Not In Instance");
-		}
-		format_type[1]++;
-		//printf("I type\n");
-		break;
-	}
 	}
 	return value;
 	printf("\n");
@@ -269,7 +274,6 @@ int memory(struct instruction_ in, int value) {
 	if (in.opcode == 0x23) { return Memory[value]; }
 	if (in.opcode == 0x2b) {
 		Memory[R[in.rs] + in.simm] = value;
-		printf("Memory address : %x, value :%x\n", R[in.rs] + in.simm, Memory[R[in.rs] + in.simm]);
 	}
 	return value;
 }
@@ -312,7 +316,6 @@ void writeback(struct instruction_ in, int input) {
 		case 0xa:
 		case 0xb: {
 			R[in.rt] = input;
-			if (in.opcode == 0x23) printf("Memory address : %x, value :%d\n", R[in.rs] + in.simm, R[in.rt]);
 			break;
 		}
 				break;
@@ -323,4 +326,15 @@ void writeback(struct instruction_ in, int input) {
 
 void update_pc() {
 	pc = pc + 4;
+}
+
+void print() {
+	int i = 0;
+	printf("실행된 명령어 수 : %d\n", cycle);
+	printf("R type 명령어 수 : %d\n", count[0]);
+	printf("I type 명령어 수 : %d\n", count[1]);
+	printf("J type 명령어 수 : %d\n", count[2]);
+	printf("Memory Access 명령어 수 : %d\n", count[3]);
+	printf("Branches 명령어 수 : %d\n\n", count[4]);
+
 }
